ce4b08e8176908c6cce013aa3d170d591ee29190
Jeferson Silva <jeferson.silva@rehagro.edu.br>
2022-10-20 10:47:44 -0300

    App\Models\Settings config file

---
+++ laravel/config/app-models-settings.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+  'secret' => [
+    'mailers.smtp.host' => '',
+    'mailers.smtp.port' => '',
+    'mailers.smtp.encryption' => '',
+    'mailers.smtp.username' => '',
+    'mailers.smtp.password' => '',
+  ],
+
+  'public' => [
+    'app.name' => '',
+    'jwt.ttl' => '',
+  ],
+];
