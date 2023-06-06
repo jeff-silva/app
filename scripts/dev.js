e97f47a0911d5a96db086c90dd2253aa1d5f6939
Jeferson Silva <jeferson.i.silva@gmail.com>
2023-06-06 12:28:29 -0300

    root init scripts

---
+++ scripts/dev.js
@@ -0,0 +1,21 @@
+const { exec } = require('child_process');
+const path = require('path');
+
+exec(
+  "docker-compose up --build --force-recreate --detach",
+  {
+    cwd: path.join(__dirname, '..'),
+    detached: true,
+  },
+  (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  }
+);
